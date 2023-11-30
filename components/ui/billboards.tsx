7d44a1e64fa9408a9d555bf9e63085ef73d967bb
Alistair <alistairm2022@gmail.com>
2023-11-30 13:48:15 +0530

    Product details page

--- components/ui/billboards.tsx
+++ components/ui/billboards.tsx
@@ -21,7 +21,7 @@ const Billboards: React.FC<BillboardProps> = ({
       autoPlay={true}
     >
       {data.map(item => (
-        <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+        <div key={item.id} className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
           <div style={{ backgroundImage: `url(${item?.imageUrl})`, backgroundColor:"#ccc" }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
             <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
               <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
