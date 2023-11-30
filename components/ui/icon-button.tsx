0fcc48eb7fa4099e926e3696915391b6ffe2088d
Alistair <alistairm2022@gmail.com>
2023-11-30 15:54:26 +0530

    Filtering on large screens

--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -3,21 +3,22 @@ import { MouseEventHandler } from "react";
 import { cn } from "@/lib/utils";
 import { Button, ButtonProps } from "./button";
 
-interface IconButtonProps {
+interface IconButtonProps extends ButtonProps {
   onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
   icon: React.ReactElement;
   className?: string;
-  props?: ButtonProps
 }
+
 
 const IconButton: React.FC<IconButtonProps> = ({
   onClick,
   icon,
   className,
-  props
+  ...props
 }) => {
   return (
     <Button
+      variant={props.variant?props.variant:"outline"}
       onClick={onClick}
       className={cn(
         'rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition',
